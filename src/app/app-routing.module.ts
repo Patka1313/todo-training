d7b0accf277768bb94a45eae09336dd33ecc08c9
patka <patrycja_kupisz@wp.pl>
2022-03-08 14:56:19 +0100

    o nas home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsPageModule } from './pages/about-us.page-module';
+import { HomePageModule } from './pages/home.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'onas', 
+        loadChildren: () => AboutUsPageModule
+      },
+  { 
+        path: '', 
+        loadChildren: () => HomePageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
