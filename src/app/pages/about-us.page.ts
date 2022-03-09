b6f7681c489266a261fe541ebf77508aee688096
patka <patrycja_kupisz@wp.pl>
2022-03-09 20:38:42 +0100

    L1E3 - nav links

--- src/app/pages/about-us.page.ts
+++ src/app/pages/about-us.page.ts
@@ -1,5 +1,12 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
-@Component({ selector: 'lib-about-us-page', templateUrl: './about-us.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({ 
+    selector: 'lib-about-us-page', 
+    templateUrl: './about-us.page.html', 
+    //encapsulation: ViewEncapsulation.None, 
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrls: ['./about-us.page.scss']
+
+})
 export class AboutUsPage {
 }
